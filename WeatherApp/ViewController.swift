e58e303462fabec9b852675ad4fd60a55d7a2a32
Admin <admin@Admins-Mac.local>
2018-03-21 22:27:21 +0300

    Third

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        print("Second")
+        print("Third")
     }
 
     override func didReceiveMemoryWarning() {
